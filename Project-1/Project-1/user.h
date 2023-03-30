bd232a193d680d9908216bc3115bbc96b49e664c
Deswald <super.jmr@yandex.ru>
2023-03-30 18:15:01 +0300

    The project is done.

--- Project-1/Project-1/user.h
+++ Project-1/Project-1/user.h
@@ -5,12 +5,23 @@ class User
 {
 public:
 	User(std::string my_name, std::string my_login, std::string my_password);
+	User();
 	~User();
-	void Sign_Up();
+
+	User& operator= (const User& str);
+
+	User sign_up();
+	User sign_in(User users[], int usersCount);
+	void sendAMessage(User users[], int usersCount, std::string username, std::string message);
+	void SendAMessageToEveryone(User users[], int usersCount, std::string message);
+	void CheckMessages(User currentUser, User users[], int usersCount);
 	void showAll();
 
-private:
+	bool _auth = false;
 	std::string _my_name;
+	std::string _my_messages[5];
+	int _message_count = 0;
+private:
 	std::string _my_login;
 	std::string _my_password;
 };
